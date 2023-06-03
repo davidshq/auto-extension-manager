bb786b22a5f9ebfe9def419500362de9c101f4c3
Dave Mackey <dave@davemackey.net>
2023-06-02 23:35:27 -0400

    Output list of installed extensions

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
-chrome.tabs.onUpdated.addListener(
-    (tabId, changeInfo, tab) => {
-      console.log('Updated to URL:', tab.url)
-    }
-  )
+// Log to console a list of all browser extensions
+const getExtensions = async (tabId, changeInfo, tab) => {
+    console.log('Updated to URL:', tab.url)
+    let extensions = await chrome.management.getAll();
+    console.log(extensions);
+}
+
+chrome.tabs.onUpdated.addListener(getExtensions);
