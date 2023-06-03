4238f335fc22b34ddc3d511b2346d6b86b2ca4ac
Dave Mackey <dave@davemackey.net>
2023-06-03 10:37:00 -0400

    Adding changed url check, domain check

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ NOTE: This plugin is being actively developed, it is not ready for use.
 To create a browser plugin that allows users to select one or more plugins that should be disabled when the browser opens one of a user selected list of domains.
 
 ## What It Does Now
-- Triggers when a tab is updated
+- Triggers when a tab is updated (but only proceeds if URL has changed)
 - Gets a list of installed extensions
-- Displays the list of installed extensions
+- Log the list of installed extensions to the console
+- Checks if the changed url is in the list of domains to disable extensions for
 - Disables an extension
 
 ## Tasks
@@ -32,3 +33,4 @@ To create a browser plugin that allows users to select one or more plugins that 
 - Allow groups of extensions to be created.
 - Allow multiple domain lists to be created.
 - Allow groups of extensions to be paired with specific domain lists so that not all plugins need be disabled on all sites whenever a block listed site is encountered.
+- Consider using Plasmo framework for browser independence.
--- background.js
+++ background.js
@@ -2,16 +2,39 @@
 const getExtensions = async (tabId, changeInfo, tab) => {
     let extensions = await chrome.management.getAll();
     console.log('Installed Extensions: ', extensions);
+    // TODO: Remove this and instead have main process return user selected list
     let extensionToDisable = extensions[0].id;
     return extensionToDisable;
 }
 
+// Triggered whenever a tab is updated
 const processTabUpdate = async (tabId, changeInfo, tab) => {
+    // We only need to take action if the URL has changed, return otherwise
+    if (!changeInfo.url) {
+        console.log('No URL change');
+        return;
+    }
     console.log('URL: ', tab.url);
-    const extensionToDisable = await getExtensions(tabId, changeInfo, tab);
-    disableExtension(extensionToDisable);
+    // Check if site is in out list of sites to disable extensions for
+    if (checkDomain(tab.url)) {
+        // Get list of extensions to disable
+        const extensionToDisable = await getExtensions(tabId, changeInfo, tab);
+        disableExtension(extensionToDisable);
+    } else {
+        console.log('Not in list of sites to disable extensions for');
+    }
 }
 
+// Check if a specific url is in list of domains to disable extensions for
+const checkDomain = (url) => {
+    const domains = ['https://www.google.com/', 'https://www.youtube.com/'];
+    if (domains.includes(url)) {
+        console.log('Domain in list of sites to disable extensions for');
+        return true
+    }
+}
+
+// Disable a specific extension
 const disableExtension = (extensionId) => {
     console.log('Extension ID: ', extensionId);
     chrome.management.setEnabled(extensionId, false, () => {
