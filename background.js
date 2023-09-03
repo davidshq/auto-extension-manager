20bae7966579c6aa6687d3f25448e7a8e00ebb32
Dave Mackey <dave@davemackey.net>
2023-09-03 17:37:18 -0400

    Minor console logging changes

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ let selectedSites = [];
 
 // Get list of extensions to disable
 const getExtensionsToDisable = () => {
-  console.log('getExtensionsToDisable');
+  console.log('Getting the list of extensions to disable');
   // Get all the items in Chrome storage
   chrome.storage.sync.get((items) => {
     // Keep the list of extensions we don't want
@@ -14,7 +14,7 @@ const getExtensionsToDisable = () => {
 
 // Get list of sites to disable extensions for
 const getSitesToDisableExtensionsFor = () => {
-  console.log('getSitesToDisableExtensionsFor');
+  console.log('Getting the list of sites to disable on');
   chrome.storage.sync.get((items) => {
     selectedSites = items.selectedWebsites;
   });
@@ -22,12 +22,11 @@ const getSitesToDisableExtensionsFor = () => {
 
 // Triggered whenever a tab is updated
 const processTabUpdate = async (tabId, changeInfo, tab) => {
-  console.log('processTabUpdate');
+  console.log('A tab update has occurred, processing...');
   // We only need to take action if the URL has changed, return otherwise
   if (!changeInfo.url) {
       return;
   }
-  console.log('URL: ', tab.url);
   // Check if site is in our list of sites to disable extensions for
   if (isDomainProhibited(tab.url)) {
     // Get list of extensions to disable
@@ -38,19 +37,18 @@ const processTabUpdate = async (tabId, changeInfo, tab) => {
     extensionsToDisable.forEach((extension) => {
       disableExtension(extension, true);
     });
-    console.log('Not in list of sites to disable extensions for');
   }
 }
 
 // Check if a specific url is in list of domains to disable extensions for
 const isDomainProhibited = (url) => {
-  console.log('isDomainProhibited');
+  console.log('Checking if domain is on prohibited list for select extensions');
   // Strip http://, subdomain, etc. from URL
   url = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "").split('/')[0];
   console.log(url);
   // Compare current tab url to urls in selected sites
   if (selectedSites.includes(url)) {
-    console.log('Domain in list of sites to disable extensions for');
+    console.log('Domain is on prohibited list');
     return true
   }
 }
@@ -60,7 +58,13 @@ const disableExtension = (extensionId, enabled) => {
   console.log('Extension ID: ', extensionId);
   chrome.management.setEnabled(extensionId, enabled, () => {
     // TODO: Add error handling
-    console.log('Extension Disabled');
+    if (enabled) {
+      console.log("Extension Enabled");
+    } else if (enabled === false) {
+      console.log("Extension Disabled");
+    } else {
+      console.log("Well, this is a bit embarrassing.");
+    }
   });
 }
 
