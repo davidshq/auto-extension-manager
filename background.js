7b2431b7b63271a2b337049a6e75b2bc945f50cb
Dave Mackey <dave@davemackey.net>
2023-09-03 16:15:47 -0400

    Significant changes, now extension utilizes actual options values instead of hard-coded.

--- background.js
+++ background.js
@@ -1,47 +1,64 @@
-// Log to console a list of all browser extensions
-const getExtensions = async (tabId, changeInfo, tab) => {
-    let extensions = await chrome.management.getAll();
-    console.log('Installed Extensions: ', extensions);
-    // TODO: Remove this and instead have main process return user selected list
-    let extensionToDisable = extensions[0].id;
-    return extensionToDisable;
+let selectedExtensions = [];
+let selectedSites = [];
+
+// Get list of extensions to disable
+const getExtensionsToDisable = () => {
+  console.log('getExtensionsToDisable');
+  chrome.storage.sync.get((items) => {
+    selectedExtensions = items.excludedExtensions;
+    console.log('Selected Extensions: ', selectedExtensions);
+  });
+}
+
+// Get list of sites to disable extensions for
+const getSitesToDisableExtensionsFor = () => {
+  console.log('getSitesToDisableExtensionsFor');
+  chrome.storage.sync.get((items) => {
+    selectedSites = items.selectedWebsites;
+  });
 }
 
 // Triggered whenever a tab is updated
 const processTabUpdate = async (tabId, changeInfo, tab) => {
-    // We only need to take action if the URL has changed, return otherwise
-    if (!changeInfo.url) {
-        console.log('No URL change');
-        return;
-    }
-    console.log('URL: ', tab.url);
-    // Check if site is in our list of sites to disable extensions for
-    if (checkDomain(tab.url)) {
-        // Get list of extensions to disable
-        const extensionToDisable = await getExtensions(tabId, changeInfo, tab);
-        disableExtension(extensionToDisable);
-    } else {
-        console.log('Not in list of sites to disable extensions for');
-    }
+  console.log('processTabUpdate');
+  // We only need to take action if the URL has changed, return otherwise
+  if (!changeInfo.url) {
+      console.log('No URL change');
+      return;
+  }
+  console.log('URL: ', tab.url);
+  // Check if site is in our list of sites to disable extensions for
+  if (isDomainProhibited(tab.url)) {
+    // Get list of extensions to disable
+    selectedExtensions.forEach((extension) => {
+      disableExtension(extension);
+    });
+  } else {
+    console.log('Not in list of sites to disable extensions for');
+  }
 }
 
 // Check if a specific url is in list of domains to disable extensions for
-const checkDomain = (url) => {
-    const domains = ['https://www.google.com/', 'https://www.youtube.com/'];
-    if (domains.includes(url)) {
-        console.log('Domain in list of sites to disable extensions for');
-        return true
-    }
+const isDomainProhibited = (url) => {
+  console.log('isDomainProhibited');
+  // Strip http://, subdomain, etc. from URL
+  url = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "").split('/')[0];
+  console.log(url);
+  if (selectedSites.includes(url)) {
+    console.log('Domain in list of sites to disable extensions for');
+    return true
+  }
 }
 
 // Disable a specific extension
 const disableExtension = (extensionId) => {
-    console.log('Extension ID: ', extensionId);
-    chrome.management.setEnabled(extensionId, false, () => {
-        // TODO: Add error handling
-        console.log('Extension Disabled');
-    });
+  console.log('Extension ID: ', extensionId);
+  chrome.management.setEnabled(extensionId, false, () => {
+    // TODO: Add error handling
+    console.log('Extension Disabled');
+  });
 }
 
+getExtensionsToDisable();
+getSitesToDisableExtensionsFor();
 chrome.tabs.onUpdated.addListener(processTabUpdate);
-
